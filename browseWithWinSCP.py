38f43ecc682839c2aa0bb5aa6c543b9496f265a1
thecotne <cotne.nazarashvili@gmail.com>
2015-01-21 22:54:28 +0400

    fix bug on 32bit windows hope not to break 64bit version

--- browseWithWinSCP.py
+++ browseWithWinSCP.py
@@ -1,19 +1,21 @@
-import sublime_plugin, sublime, subprocess
-
+import sublime_plugin, sublime, subprocess, os
 from .dirConfig import getConfig
 
-import os.path
-
-sublime.load_settings('sublime-WinSCP.sublime-settings')
-
-winscpPath = 'set PATH = %PATH%;%programfiles(x86)%\WinSCP;%programfiles%\WinSCP && '
 configName = 'sftp-config.json'
 
-def winscpCommand(conf):
-	return 'WinSCP.exe {type}://{user}:{password}@{host}:{port}"{remote_path}"'.format(**conf)
+for programFilesVar in ['ProgramFiles', 'ProgramFiles(x86)']:
+	try:
+		_winscpExe = os.environ[programFilesVar] + '\WinSCP\WinSCP.exe'
+		if os.path.exists(_winscpExe):
+			winscpExe = _winscpExe
+			break
+	except KeyError:
+		pass
+
+startWinscpCommand = '"'+ winscpExe + '"' + ' {type}://{user}:{password}@{host}:{port}"{remote_path}"'
 
 class browse_with_winscpCommand(sublime_plugin.WindowCommand):
 	def run(self, edit = None):
 		conf = getConfig(configName)
 		if conf is not None:
-			subprocess.Popen(winscpPath+winscpCommand(conf), shell=True)
+			subprocess.Popen(startWinscpCommand.format(**conf), shell=True)
