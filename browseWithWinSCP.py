59b236e9e01177b3e618b22f76684fb8edaff8d8
thecotne <cotne@itdc.ge>
2015-09-25 13:55:21 +0400

    add linux compatibility

--- browseWithWinSCP.py
+++ browseWithWinSCP.py
@@ -1,24 +1,24 @@
-import sublime_plugin, sublime, subprocess, os
+import sublime_plugin, sublime, subprocess, os, sys
 from .dirConfig import getConfig
 
 configName = 'sftp-config.json'
-winscpExe = None
+winscpExe = 'WinSCP'
 
-for programFilesVar in ['ProgramFiles', 'ProgramFiles(x86)']:
-	try:
-		_winscpExe = os.environ[programFilesVar] + '\WinSCP\WinSCP.exe'
-		if os.path.exists(_winscpExe):
-			winscpExe = _winscpExe
-			break
-	except KeyError:
-		pass
+if sys.platform.startswith('win'):
+	for programFilesVar in ['ProgramFiles', 'ProgramFiles(x86)']:
+		try:
+			_winscpExe = os.environ[programFilesVar] + '\WinSCP\WinSCP.exe'
+			if os.path.exists(_winscpExe):
+				winscpExe = _winscpExe
+				break
+		except KeyError:
+			pass
 
-if winscpExe:
-	startWinscpCommand = '"'+ winscpExe + '"' + ' {type}://{user}:{password}@{host}:{port}"{remote_path}" /rawsettings LocalDirectory="{local_path}"'
+startWinscpCommand = '"'+ winscpExe + '"' + ' {type}://{user}:{password}@{host}:{port}"{remote_path}" /rawsettings LocalDirectory="{local_path}"'
 
-	class browse_with_winscpCommand(sublime_plugin.WindowCommand):
-		def run(self, edit = None):
-			conf = getConfig(configName)
-			if conf is not None:
-				subprocess.Popen(startWinscpCommand.format(**conf), shell=True)
+class browse_with_winscpCommand(sublime_plugin.WindowCommand):
+	def run(self, edit = None):
+		conf = getConfig(configName)
+		if conf is not None:
+			subprocess.Popen(startWinscpCommand.format(**conf), shell=True)
 
