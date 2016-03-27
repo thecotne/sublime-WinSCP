a9d1942c9b56917439a07fe8b808ea4727d3268a
cotne nazarashvili <cotne.95@gmail.com>
2016-03-27 14:32:48 +0400

    make osx compatible

--- browseWithWinSCP.py
+++ browseWithWinSCP.py
@@ -1,8 +1,10 @@
 import sublime_plugin, sublime, subprocess, os, sys
 from .dirConfig import getConfig
 
+settings = sublime.load_settings('WinSCP.sublime-settings');
+
 configName = 'sftp-config.json'
-winscpExe = 'WinSCP'
+winscpExe = settings.get('winscpExe') or 'WinSCP';
 
 if sys.platform.startswith('win'):
 	for programFilesVar in ['ProgramFiles', 'ProgramFiles(x86)']:
@@ -15,6 +17,9 @@ if sys.platform.startswith('win'):
 			pass
 
 startWinscpCommand = '"'+ winscpExe + '"' + ' {type}://"{user}":"{password}"@{host}:{port}"{remote_path}" /rawsettings LocalDirectory="{local_path}"'
+
+if sys.platform == 'darwin':
+	startWinscpCommand = "/Applications/Wine.app/Contents/Resources/bin/wine  " + startWinscpCommand
 
 class browse_with_winscpCommand(sublime_plugin.WindowCommand):
 	def run(self, edit = None):
